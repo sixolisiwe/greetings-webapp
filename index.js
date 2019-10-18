354b94d623dc789991c24037b9c2fb2a5d8033ad
sixolisiwe <sxlswmcb@gmail.com>
2019-10-18 11:38:23 +0200

    heroku pool added

--- index.js
+++ index.js
@@ -15,11 +15,12 @@ const session = require('express-session');
 const pg = require("pg");
 const Pool = pg.Pool;
 
-// we are using a special test database for the tests
-const connectionString = process.env.DATABASE_URL || 'postgresql://codex:codex123@localhost:5432/GreetingsApp';
+
+const connectionString = process.env.DATABASE_URL || 'postgresql://codex:codex123@localhost:5432/postgres://thsbnfgjjqqfbo:3f6718d79d0935d7304ce3a4c0fc2cc2980856bcf3dd1336445049281aa74b78@ec2-23-21-148-223.compute-1.amazonaws.com:5432/d6i3sd32rjjbeh';
 
 const pool = new Pool({
     connectionString
+
 });
 
 
