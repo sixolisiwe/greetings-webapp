42bcc9ce4eecb2505d096a363c55dfcba65790f0
sixolisiwe <sxlswmcb@gmail.com>
2019-10-18 14:36:46 +0200

    fixed travis file

--- test/greet.test.js
+++ test/greet.test.js
@@ -4,7 +4,7 @@ const pg = require("pg");
 const Pool = pg.Pool;
 
 // we are using a special test database for the tests
-const connectionString = process.env.DATABASE_URL || 'postgresql://codex:codex123@localhost:5432/greet_test';
+const connectionString = process.env.DATABASE_URL || 'postgresql://postgres@localhost:5432/greet_test';
 
 const pool = new Pool({
     connectionString,
